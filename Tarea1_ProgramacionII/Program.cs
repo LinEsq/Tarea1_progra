2223e81c1c997d0d6275990b790692be92021a81
Linsy Esquivel <lmesquivel.28@icloud.com~>
2025-10-04 22:12:55 -0600

    tarea1 actualizada

--- Tarea1_ProgramacionII/Program.cs
+++ Tarea1_ProgramacionII/Program.cs
@@ -7,149 +7,170 @@ using System.Threading.Tasks;
 
 namespace Tarea1_ProgramacionII
 {
-    internal class Program
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Text;
+    using System.Threading.Tasks;
+
+
+    namespace Tarea1_ProgramacionII
     {
-        static void Main(string[] args)
+        class Program
         {
-            
-            datos_empleado();
-
-        }
-        static void datos_empleado()
-        {
-            int i = 0;
-
-            int Cantidad_operarios = 0;
-            double SalarioNetoOperarios = 0;
-            double PromedioSalarioOperarios = 0;
-
-            int Cantidad_tecnicos = 0;
-            double SalarioNetoTecnicos = 0;
-            double PromedioSalarioTecnicos = 0;
-
-            int Cantidad_profesionales = 0;
-            double SalarioNetoProfesionales = 0;
-            double PromedioSalarioProfesionales = 0;
 
 
-            do
+            // Aumento segun tipo de empleado
+            static double Aumento(double salarioOrdinario, byte tipoEmpleado)
             {
-                Console.WriteLine("Ingrese su numero de cedula ");
-                string cedula = Console.ReadLine();
-
-                Console.WriteLine("Ingrese su nombre ");
-                string nombre = Console.ReadLine();
-
-                Console.WriteLine("Cuantas fueron sus horas laboradas? ");
-                double horas_laboradas = double.Parse(Console.ReadLine());
-
-                Console.WriteLine("Ingrese el precio por hora laborada ");
-                double precio_hora = double.Parse(Console.ReadLine());
-
-                double salario_ordinario = (horas_laboradas * precio_hora);
-
-
-                Console.WriteLine("Digite el numero del tipo de empleado que usted labora:  \n 1 - Operario. \n 2 - Tecnico. \n 3 - Profesional");
-                byte tipo_empleado = byte.Parse(Console.ReadLine());
-
-                switch (tipo_empleado)
+                switch (tipoEmpleado)
                 {
-                    case 1:
-                        Cantidad_operarios+=1;
-                        double aumento1 = (salario_ordinario * 0.15);
-                        double salario_bruto1 = (salario_ordinario + aumento1);
-                        double DeduccionesLey1 = (salario_bruto1*0.0917);
-                        double salario_neto1 = salario_bruto1 - DeduccionesLey1;
-                        SalarioNetoOperarios = SalarioNetoOperarios + salario_neto1;
-
-                        Console.WriteLine($" Cedula:{cedula} \n Nombre del empleado:{nombre} \n Tipo de empleado:{tipo_empleado} \n Salario por hora:{precio_hora} \n Cantidad de horas:{horas_laboradas} \n Salario ordinario:{precio_hora*horas_laboradas} \n Aumento por 15%:{aumento1} \n Salario bruto:{salario_bruto1} \n Deduccion CCSS:{DeduccionesLey1} \n Salario Neto: {salario_neto1}");
-
-                        Console.WriteLine("Desea ingresar otro empleado? Si/No");
-                        string respuesta = Console.ReadLine();
-                        if (respuesta.ToLower() == "si")
-                        {
-                            break;
-                        }
-                        else
-                        {
-                            if (respuesta.ToLower() == "no")
-                            {
-                                i = 1;
-
-                            }
-                        }
-                        break;
-
-                    case 2:
-                        Cantidad_tecnicos += 1;
-                        double aumento2 = (salario_ordinario * 0.10);
-                        double salario_bruto2 = (salario_ordinario + aumento2);
-                        double DeduccionesLey2 = (salario_bruto2 * 0.0917);
-                        double salario_neto2 = salario_bruto2 - DeduccionesLey2;
-                        SalarioNetoTecnicos = SalarioNetoOperarios + salario_neto2;
-
-                        Console.WriteLine($" Cedula:{cedula} \n Nombre del empleado:{nombre} \n Tipo de empleado:{tipo_empleado} \n Salario por hora:{precio_hora} \n Cantidad de horas:{horas_laboradas} \n Salario ordinario:{precio_hora * horas_laboradas} \n Aumento por 10%:{aumento2} \n Salario bruto:{salario_bruto2} \n Deduccion CCSS:{DeduccionesLey2} \n Salario Neto: {salario_neto2}");
-
-                        Console.WriteLine("Desea ingresar otro empleado? Si/No");
-                        string respuesta2 = Console.ReadLine();
-                        if (respuesta2.ToLower() == "si")
-                        {
-                            break;
-                        }
-                        else
-                        {
-                            if (respuesta2.ToLower() == "no")
-                            {
-                                i = 1;
-
-                            }
-                        }
-                        break;
-
-                    case 3:
-                        Cantidad_profesionales += 1;
-                        double aumento3 = (salario_ordinario * 0.05);
-                        double salario_bruto3 = (salario_ordinario + aumento3);
-                        double DeduccionesLey3 = (salario_bruto3 * 0.0917);
-                        double salario_neto3 = salario_bruto3 - DeduccionesLey3;
-                        SalarioNetoProfesionales = SalarioNetoOperarios + salario_neto3;
-
-                        Console.WriteLine($" Cedula:{cedula} \n Nombre del empleado:{nombre} \n Tipo de empleado:{tipo_empleado} \n Salario por hora:{precio_hora} \n Cantidad de horas:{horas_laboradas} \n Salario ordinario:{precio_hora * horas_laboradas} \n Aumento por 5%:{aumento3} \n Salario bruto:{salario_bruto3} \n Deduccion CCSS:{DeduccionesLey3} \n Salario Neto: {salario_neto3}");
-
-                        Console.WriteLine("Desea ingresar otro empleado? Si/No");
-                        string respuesta3 = Console.ReadLine();
-                        if (respuesta3.ToLower() == "si")
-                        {
-                            break;
-                        }
-                        else
-                        {
-                            if (respuesta3.ToLower() == "no")
-                            {
-                                i = 1;
-
-                            }
-                        }
-                        break;
+                    case 1: return salarioOrdinario * 0.15; // Operario
+                    case 2: return salarioOrdinario * 0.10; // Tecnico
+                    case 3: return salarioOrdinario * 0.05; // Profesional
+                    default: return 0;
                 }
+            }
 
-            } while (i != 1);
+            // Salario bruto 
+            static double SalarioBruto(double salarioOrdinario, byte tipoEmpleado)
+            {
+                return salarioOrdinario + Aumento(salarioOrdinario, tipoEmpleado);
+            }
 
-            Console.WriteLine($"Cantidad de empleados tipo operarios: {Cantidad_operarios}");
-            Console.WriteLine($"Acomulado de salarios netos de operarios: {SalarioNetoOperarios}");
-            PromedioSalarioOperarios = SalarioNetoOperarios/Cantidad_operarios;
-            Console.WriteLine($"Promedio de salario para Operarios: {PromedioSalarioOperarios}");
+            // Deduccion de la ley
+            static double Deduccion(double salarioBruto)
+            {
+                return salarioBruto * 0.0917;
+            }
 
-            Console.WriteLine($"Cantidad de empleados tipo tecnicos: {Cantidad_tecnicos}");
-            Console.WriteLine($"Acomulado de salarios netos de tecnicos: {SalarioNetoTecnicos}");
-            PromedioSalarioTecnicos = SalarioNetoTecnicos / Cantidad_tecnicos;
-            Console.WriteLine($"Promedio de salario para tecnicos: {PromedioSalarioTecnicos}");
+            // Salario neto 
+            static double SalarioNeto(double salarioOrdinario, byte tipoEmpleado)
+            {
+                double bruto = SalarioBruto(salarioOrdinario, tipoEmpleado);
+                double deduccion = Deduccion(bruto);
+                return bruto - deduccion;
+            }
 
-            Console.WriteLine($"Cantidad de empleados tipo profesionales: {Cantidad_profesionales}");
-            Console.WriteLine($"Acomulado de salarios netos de profesionales: {SalarioNetoProfesionales}");
-            PromedioSalarioProfesionales = SalarioNetoProfesionales / Cantidad_profesionales;
-            Console.WriteLine($"Promedio de salario para profesionales: {PromedioSalarioProfesionales}");
+            // funcion principal
+            static void Main()
+            {
+                Menu();
+            }
+
+            // 
+            static void Menu()
+            {
+                byte opcion;
+                do
+                {
+                    Console.WriteLine("Que opcion te gustaria realizar?");
+                    Console.WriteLine("1: Ingresar empleados y ver calculos");
+                    Console.WriteLine("2: Salir del sistema");
+
+                    opcion = byte.Parse(Console.ReadLine());
+
+                    switch (opcion)
+                    {
+                        case 1:
+                            datos_empleado();
+                            break;
+
+                        case 2:
+                            Console.WriteLine("Has salido del sistema de empleados.");
+                            return;
+
+                        default:
+                            Console.WriteLine("Opción no valida.");
+                            break;
+                    }
+
+                } while (opcion != 2);
+            }
+
+            // funciones
+            static void datos_empleado()
+            {
+                int i = 0;
+
+                int Cantidad_operarios = 0;
+                double SalarioNetoOperarios = 0;
+
+                int Cantidad_tecnicos = 0;
+                double SalarioNetoTecnicos = 0;
+
+                int Cantidad_profesionales = 0;
+                double SalarioNetoProfesionales = 0;
+
+                do
+                {
+                    Console.WriteLine("Ingrese su numero de cedula: ");
+                    string cedula = Console.ReadLine();
+
+                    Console.WriteLine("Ingrese su nombre: ");
+                    string nombre = Console.ReadLine();
+
+                    Console.WriteLine("Cuantas fueron sus horas laboradas? ");
+                    double horas_laboradas = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Ingrese el precio por hora laborada: ");
+                    double precio_hora = double.Parse(Console.ReadLine());
+
+                    double salario_ordinario = (horas_laboradas * precio_hora);
+
+                    Console.WriteLine("Digite el numero de empleado que labora actualmente:  \n 1 - Operario \n 2 - Tecnico \n 3 - Profesional");
+                    byte tipo_empleado = byte.Parse(Console.ReadLine());
+
+
+                    double aumento = Aumento(salario_ordinario, tipo_empleado);
+                    double salario_bruto = SalarioBruto(salario_ordinario, tipo_empleado);
+                    double deducciones = Deduccion(salario_bruto);
+                    double salario_neto = SalarioNeto(salario_ordinario, tipo_empleado);
+
+
+                    switch (tipo_empleado)
+                    {
+                        case 1:
+                            Cantidad_operarios++;
+                            SalarioNetoOperarios += salario_neto;
+                            break;
+                        case 2:
+                            Cantidad_tecnicos++;
+                            SalarioNetoTecnicos += salario_neto;
+                            break;
+                        case 3:
+                            Cantidad_profesionales++;
+                            SalarioNetoProfesionales += salario_neto;
+                            break;
+                    }
+
+                    Console.WriteLine($"\nResumen del empleado:");
+                    Console.WriteLine($"\nCédula: {cedula} \n Nombre: {nombre} \n Tipo de empleado: {tipo_empleado} \n Salario por hora: {precio_hora} \n Salario ordinario:{salario_ordinario} \n Aumento: {aumento} \n Salario bruto: {salario_bruto}\n Deducciones CCSS: {deducciones} \n Salario Neto: {salario_neto}");
+
+                    Console.WriteLine("\nTe gustaria ingresar otro empleado? (Si/No)");
+                    string respuesta = Console.ReadLine().ToLower();
+                    if (respuesta == "no")
+                    {
+                        i = 1;
+                    }
+
+                } while (i != 1);
+
+
+                Console.WriteLine(" ___ RESULTADOS FINALES ___ ");
+
+                if (Cantidad_operarios > 0)
+                    Console.WriteLine($"Operarios: Cantidad: {Cantidad_operarios}, Promedio Neto: {SalarioNetoOperarios / Cantidad_operarios}");
+                if (Cantidad_tecnicos > 0)
+                    Console.WriteLine($"Tecnicos: Cantidad: {Cantidad_tecnicos}, Promedio Neto: {SalarioNetoTecnicos / Cantidad_tecnicos}");
+                if (Cantidad_profesionales > 0)
+                    Console.WriteLine($"Profesionales: Cantidad: {Cantidad_profesionales}, Promedio Neto: {SalarioNetoProfesionales / Cantidad_profesionales}");
+            }
         }
-       
     }
+
+
 }
+
+       
